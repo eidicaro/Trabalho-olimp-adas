ae8a6a9a3473dd9e7ddfed6d9334d94d3e598bb7
EnzoPaes <paesenzo10@gmail.com>
2024-09-02 13:55:59 -0300

    Protótipo Header

---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/header.css">
+</head>
+<body>
+
+    <header>
+        <div class="logo">
+
+        </div>
+
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">História</a></li>
+                <li><a href="">Modalidades</a>
+                    <ul class="dropdown">
+                        <li><a href="">Salto</a></li>
+                        <li><a href="">Corridas</a></li>
+                        <li><a href="">Arremesso</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+
+        <button class="btn-feedback">
+
+        </button>
+    </header>
+
+</body>
+</html>
