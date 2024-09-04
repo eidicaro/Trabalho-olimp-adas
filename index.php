ba85998c71743f3ecb9894c492b551f4338c3bbd
eidicaro <icaroeid@gmail.com>
2024-09-04 14:43:49 -0300

    implanatação da fonte

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
     <div class="intro">
         <div class="text-intro">
-        <h1 class="slogan">O Melhor Portal de Atletismo</h1>
+        <h1 class="slogan"><b>O Melhor Portal de Atletismo</b></h1>
         <br>
         <p class="description">
            Aqui você pode explorar 14 modalidades emocionantes do esporte, desde corridas rápidas até saltos e arremessos. Descubra a história fascinante do atletismo, tanto globalmente quanto no Brasil, e veja como o esporte evoluiu ao longo dos anos. Acompanhe os feitos históricos dos maiores atletas e conheça o impacto do atletismo em diferentes competições. Navegue e mergulhe no mundo do atletismo conosco! 
