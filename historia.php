f24c4846d7e9737f85b3ce9f214e26d0a5d74ae4
eidicaro <icaroeidgmail.com>
2024-09-09 14:35:09 -0300

    estilização da pag história

--- historia.php
+++ historia.php
@@ -14,9 +14,20 @@
     ?>
 
     <div class="history">
-        
-    </div>
+        <h1>História</h1>
+        <br>
+        <p>O atletismo, que surgiu há 4 mil anos no Egito com corridas entre homens, evoluiu para se tornar o destaque das Olimpíadas Antigas na Grécia. No século XIX, na Inglaterra, o esporte ganhou popularidade com corridas organizadas de forma irregular. Em 1825, foram realizadas competições amadoras em Uxbridge, e, em 1827, a corrida com obstáculos foi criada em Eton. Em 1834, surgiram as primeiras regras oficiais para algumas provas, mas o primeiro torneio oficial só ocorreu em 1864, com as competições anuais entre as universidades de Oxford e Cambridge.</p>
+        <br>
+        <p>O atletismo faz parte das Olimpíadas Modernas desde a primeira edição em 1896, na Grécia. A Federação Internacional de Atletismo Amador (IAAF), que regula o esporte até hoje, foi fundada em 1913, em Londres. Geralmente praticado em estádios, o atletismo também inclui corridas de longa distância, como a maratona, realizadas em vias públicas ou no campo. Considerado o esporte mais antigo e nobre, o atletismo serve como base para diversas outras modalidades esportivas</p>
 
+
+        <h1>História No Brasil</h1>
+        <br>
+        <p>O atletismo no Brasil começou a se desenvolver nas primeiras décadas do século XX. A Confederação Brasileira de Desportos (CBD) juntou-se à Associação Internacional de Federações de Atletismo (IAAF) em 1914, e a primeira participação brasileira nas Olimpíadas foi em Paris, 1924. O Campeonato Brasileiro de Atletismo iniciou em 1925 e a Confederação Brasileira de Atletismo (CBAt) foi criada em 1977.</p>
+        <br>
+        <p>O Brasil conquistou seu primeiro ouro no atletismo nas Olimpíadas de Helsinque em 1952, com Adhemar Ferreira da Silva no salto triplo. Destaques brasileiros incluem Maurren Maggi, ouro no salto em distância em 2008; Thiago Braz, ouro no salto com vara em 2016; e Vanderlei Cordeiro, bronze na maratona em 2004. O atletismo é uma das modalidades com mais medalhas olímpicas brasileiras, com 19 até os Jogos de Tóquio 2021.</p>
+
+    </div>
 
     <?php
         include_once('includes/footer.php')
