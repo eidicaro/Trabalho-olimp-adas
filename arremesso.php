5f20a151377b12a891cdd49203ca2737322936ee
eidicaro <icaroeid@gmail.com>
2024-09-17 16:13:46 -0300

    adicao de conteudos

--- arremesso.php
+++ arremesso.php
@@ -12,7 +12,27 @@
     <?php
         include_once('includes/header.php');
     ?>
+    <div class="arremesso">
+        <h1><b>Arremesso</b></h1>
 
+        <h2 class="subtitle">Arremesso de Peso</h2>
+        <div class="arremesso-peso">
+        <p>O arremesso de peso é uma prova do atletismo em que o atleta deve lançar uma esfera de metal, chamada de peso, o mais longe possível. A técnica envolve força, explosão e precisão. O atleta realiza o lançamento dentro de uma área circular, utilizando movimentos específicos para ganhar impulso, como o estilo de giro ou o de deslocamento lateral. O peso é arremessado com uma mão a partir do ombro, e a distância é medida do ponto de saída até o local em que o peso toca o solo. Vence quem conseguir a maior distância no arremesso, dentro das regras e limites estabelecidos.</p>
+        <img src="../trabalho-olimpiadas/images/arremesso-peso.jpg" alt="imagem arremesso de peso">
+        </div>
+
+        <h2  class="subtitle">Arremesso de Disco</h2>
+        <div class="arremesso-disco">
+        <p>O arremesso de disco é uma modalidade do atletismo em que o atleta lança um disco metálico o mais longe possível. O objetivo é alcançar a maior distância com um arremesso preciso e controlado. O atleta começa com uma rotação dentro de um círculo de lançamento, utilizando a força das pernas e a técnica adequada para impulsionar o disco. O arremesso é medido a partir da linha de saída até o ponto onde o disco toca o solo. É um esporte que combina força, técnica e coordenação.</p>
+        <img src="../trabalho-olimpiadas/images/arremesso-disco.jfif" alt="imagem arrremesso de disco">
+        </div>
+
+        <h2  class="subtitle">Arremesso de Dardo</h2>
+        <div class="arremesso-dardo">
+        <p>O arremesso de dardo é uma prova do atletismo onde o objetivo é lançar um dardo (um objeto longo e fino) o mais longe possível. O atleta corre dentro de uma área de decolagem e, ao final da pista, faz um arremesso usando a força do braço e a técnica adequada para projetar o dardo. O arremesso é medido a partir da linha de saída até o ponto onde o dardo toca o solo. A técnica correta é crucial para maximizar a distância, e a competição avalia tanto a precisão quanto a distância dos arremessos.</p>
+        <img src="../trabalho-olimpiadas/images/arremesso-dardo.jfif" alt="imagem arremesso de dardo">
+        </div>
+    </div>
     <?php
         include_once('includes/footer.php')
     ?>
--- corridas.php
+++ corridas.php
@@ -12,7 +12,45 @@
     <?php
         include_once('includes/header.php');
     ?>
+    <div class="corrida">
+    <h1><b>Corridas</b></h1>
 
+    <h2 class="subtitle">100m Rasos</h2>
+        <div class="m100-rasos">
+        <p>Os 100 metros rasos são uma das provas mais rápidas e icônicas do atletismo. Nela, os atletas correm em linha reta por uma pista de 100 metros, buscando atingir a maior velocidade possível desde o início. A prova exige explosão, força e técnica, sendo decidida em frações de segundo. Vence o atleta que cruzar a linha de chegada primeiro. Considerada a prova que coroa o "homem mais rápido do mundo", os 100 metros rasos são destaque em competições como Olimpíadas e campeonatos mundiais.</p>
+        <img src="../trabalho-olimpiadas/images/100m-rasos.jfif" alt="imagem 100m rasos">
+        </div>
+
+        <h2 class="subtitle">110m com Barreira</h2>
+        <div class="m110-barreira">
+        <p>Os 110 metros com barreiras são uma prova do atletismo masculino em que os atletas correm em alta velocidade enquanto superam 10 barreiras distribuídas ao longo da pista. A prova combina explosão, velocidade e técnica, pois o atleta precisa manter o ritmo de corrida enquanto realiza saltos precisos sobre as barreiras, sem perder tempo. A altura das barreiras é de 1,067 metros, e a distância entre elas é de 9,14 metros. Vence quem completar o percurso no menor tempo possível, equilibrando agilidade e coordenação durante os saltos e as passadas.</p>
+        <img src="../trabalho-olimpiadas/images/110m-barreira.jpg" alt="imagem 110m com barreira">
+        </div>
+
+        <h2 class="subtitle">200m Rasos</h2>
+        <div class="m200-rasos">
+        <p>Os 200 metros rasos são uma prova de velocidade no atletismo em que os atletas correm em uma curva seguida por uma reta de 200 metros. A prova exige tanto explosão quanto resistência à velocidade, já que os corredores precisam manter um ritmo forte durante toda a distância, especialmente ao sair da curva e acelerar na reta final. A largada é feita em posições escalonadas devido à curva da pista. Vence quem completar o percurso no menor tempo, com destaque para a técnica de corrida e a transição perfeita da curva para a reta.</p>
+        <img src="../trabalho-olimpiadas/images/200m-rasos.jpg" alt="imagem 200m rasos">
+        </div>
+
+        <h2 class="subtitle">400m Rasos</h2>
+        <div class="m400-rasos">
+        <p>Os 400 metros rasos são uma prova de atletismo de velocidade em que os atletas correm uma volta completa em torno da pista. Considerada uma combinação de velocidade e resistência, a prova exige que os corredores mantenham um ritmo intenso ao longo dos 400 metros, equilibrando força e estratégia para evitar a fadiga na reta final. A largada é feita em posições escalonadas devido à curva da pista. Vence o atleta que cruzar a linha de chegada primeiro, sendo uma das provas mais desafiadoras do atletismo em termos de esforço físico e controle de ritmo.</p>
+        <img src="../trabalho-olimpiadas/images/400m-rasos.jpg" alt="imagem 400m rasos">
+        </div>
+
+        <h2 class="subtitle">400m Com Barra</h2>
+        <div class="m400-barra">
+        <p>Os 400 metros com barreiras são uma prova do atletismo em que os atletas percorrem uma volta completa na pista enquanto saltam 10 barreiras distribuídas ao longo do percurso. A prova combina velocidade, resistência e técnica, já que os corredores precisam manter um ritmo forte enquanto realizam saltos precisos. As barreiras têm 91,4 cm de altura no masculino e 76,2 cm no feminino. Além de controlar a velocidade, o atleta precisa gerenciar a fadiga e coordenar suas passadas para superar as barreiras sem perder ritmo. Vence quem completar o percurso no menor tempo possível.</p>
+        <img src="../trabalho-olimpiadas/images/400m-barreira.jpg" alt="imagem 400m com barra">
+        </div>
+
+        <h2 class="subtitle">4x100m Revezamento</h2>
+        <div class="revezamento">
+        <p>O revezamento 4x100 metros é uma prova de velocidade em equipe no atletismo, composta por quatro atletas que correm 100 metros cada um. A corrida é realizada em sequência, com cada atleta passando um bastão para o próximo dentro de uma zona específica de 20 metros. A troca precisa ser rápida e precisa, pois erros ou perda de tempo nas passagens podem comprometer o desempenho da equipe. A largada e as trocas são feitas em posições escalonadas devido à curva da pista. Vence a equipe que completar o percurso no menor tempo, unindo velocidade, coordenação e trabalho em equipe.</p>
+        <img src="../trabalho-olimpiadas/images/revezamento.jpg" alt="imagem 4x100 Revezamento">
+        </div>
+    </div>
     <?php
         include_once('includes/footer.php')
     ?>
