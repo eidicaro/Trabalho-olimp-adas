60d5ee3061694d97d8d0d059333b5cf10246e889
Victor Hugo Mizutani <victorhg.mizutani@gmail.com>
2024-09-22 11:56:19 -0300

    Logo
    Coloquei a logo na aba

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Runnin'</title>
+    <link rel="icon" href="../trabalho-olimpiadas/images/logo_nossa.png" type="image/png">
     <link rel="stylesheet" href="assets/style.css">
     <link rel="stylesheet" href="assets/header.css">
     <link rel="stylesheet" href="assets/footer.css">
